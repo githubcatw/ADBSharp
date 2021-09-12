08407ad660260bac0701aaafc8f71866a252ebcb
Narek Torosyan <narek.email@gmail.com>
2021-09-12 20:20:01 +0400

    Add more high level commands

--- ADB/HighLevelADB.cs
+++ ADB/HighLevelADB.cs
@@ -25,5 +25,31 @@ namespace NUDev.ADBSharp {
         public static void DownloadFile(this ADB adb, string localPath, string destPath) {
             adb.Run("pull " + localPath + " " + destPath);
         }
+
+        /// <summary>
+        /// Upload a file.
+        /// </summary>
+        /// <param name="localPath">The path of the file on this machine.</param>
+        /// <param name="destPath">The destination path on the phone.</param>
+        public static void UploadFile(this ADB adb, string localPath, string destPath) {
+            adb.Run("push " + localPath + " " + destPath);
+        }
+
+        /// <summary>
+        /// Restarts the ADB server.
+        /// </summary>
+        public static void RestartServer(this ADB adb) {
+            adb.Run("start-server");
+            adb.Run("kill-server");
+        }
+
+        /// <summary>
+        /// Lists devices.
+        /// </summary>
+        public static string[] Devices(this ADB adb) {
+            var devices = adb.Run("devices").Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (devices[0].StartsWith("* daemon")) return devices.Skip(3).ToArray();
+            else return devices.Skip(1).ToArray();
+        }
     }
 }
