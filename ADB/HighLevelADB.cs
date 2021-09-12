097ad93fa6434090069c42d772e3c6c2b4ea3c13
Narek Torosyan <narek.email@gmail.com>
2021-09-12 20:20:10 +0400

    Update HighLevelADB.cs

--- ADB/HighLevelADB.cs
+++ ADB/HighLevelADB.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NUDev.ADBSharp {
+namespace NUDev.ADBSharp.HL {
     public static class HighLevelADB {
 
         /// <summary>
